9fedf7df4f7f2c1397b813f4b39912df1aefe087
Егор <ealekseev332@gmail.com>
2024-02-28 20:10:48 +0300

    Реализовал подлкючение класса Random и цикла while (1-ая задача)

--- MDK02.02/Program.cs
+++ MDK02.02/Program.cs
@@ -8,9 +8,27 @@ namespace MDK02._02
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Головной модуль
+        static void Main()
         {
+            //Подключение класса Random.
+            Random random = new Random();
+            //Заполнение класса Random числами до 100.
+            int zagadannoeChislo = random.Next(0, 101);
+            //Изначальное количество попыток, равно 0.
+            int attempts = 0;
+            //Количество догадок.
+            int guess = -1;
 
+            Console.WriteLine("Отгадайте число от 0 до 100. У вас 10 попыток.");
+
+            //Цикл while, в котором устанавливается конкретное количество попыток и происходит их увелечение на один, пока не достигнет 10.
+            while (guess != zagadannoeChislo && attempts < 10)
+            {
+                Console.Write("Попытка №" + (attempts + 1) + ". Введите ваше предположение: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                attempts++;
+            }
         }
     }
 }
